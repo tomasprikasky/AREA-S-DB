12406253dee1e272fcda018009cbf015f7216d11
tomasprikasky <tom.prikasky@seznam.cz>
2023-01-27 23:04:02 +0100

    inicializace projektu

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZAD_WEB_2b.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
